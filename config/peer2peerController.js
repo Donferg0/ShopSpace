05dea9a31c8ee2f419d454e5a5d0ee9d6b062639
BlazePhoenix <donnel.ferguson@hotmail.com>
2019-05-14 13:40:05 -0400

    DB creation

---
+++ config/peer2peerController.js
@@ -0,0 +1,65 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (items.js) to use its database functions.
+var peer = require("../models/items.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function (req, res) {
+    items.all(function (data) {
+        var hbsObject = {
+            peer2peer: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/", function (req, res) {
+    items.create(["price", "name", "image", "quantity", "category"
+    ], [
+            req.body.price, req.body.name, req.body.image, req.body.quantity, req.body.category], function (result) {
+                // Send back the ID of the new quote
+                res.redirect("/");
+            });
+});
+
+router.put("/:name", function (req, res) {
+    var condition = "name = " + req.params.name;
+
+    console.log("condition", condition);
+
+    peer2peer.update(
+        {
+            name: req.body.name,
+            price: req.body.price,
+            quantity: req.body.quantity,
+            image: req.body.ame,
+            category: req.body.category,
+        }, function () {
+            if (result.changedRows == 0) {
+                // If no rows were changed, then the ID must not exist, so 404
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+                res.redirect("/");
+            }
+        });
+});
+
+router.delete("/api/peer2peer/:name", function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    peer.delete(condition, function (result) {
+        if (result.affectedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
