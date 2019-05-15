cb809cf575e4557df44ca0f67d4f2956fe30ec6e
Tyawo <48452575+Tyawo@users.noreply.github.com>
2019-05-15 10:31:37 -0400

    update users

--- models/users.js
+++ models/users.js
@@ -2,6 +2,16 @@ module.exports = function(sequelize, DataTypes) {
     var users = sequelize.define("users", {
       email: DataTypes.STRING,
       password: DataTypes.TEXT
+
     });
+    
+      users.associate = function(models) {
+        // Associating Author with Posts
+        // When an Author is deleted, also delete any associated Posts
+        users.hasMany(models.items, {
+          onDelete: "cascade"
+        });
+      };
+    
     return users;
   };
