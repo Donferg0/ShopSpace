c188b168395fee76c76b0bf2a8b183fc566cbb58
Tyawo <48452575+Tyawo@users.noreply.github.com>
2019-05-16 14:46:56 -0400

    update apiroutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,9 +9,46 @@ module.exports = function(app) {
   });
 
   // Get route for returning items of a specific category
+  app.get("/buynow", function(req, res) {
+    // Add sequelize code to find all posts where the category is equal to req.params.category,
+    // return the result to the user with res.json
+    var price = req.body.item.price;
+    var id = req.body.item.id;
+    var purchasedQuantity = req.body.item.quantity;
+    var total = price * purchasedQuantity;
+    db.items
+      .findAll({
+        where: {
+          id: id
+        }
+      })
+      .then(function(response) {
+        var updatedquantity = response.quantity - purchasedQquantity;
+        db.items
+          .update(
+            {
+              quantity: updatedquantity
+            },
+            {
+              where: {
+                id: id
+              }
+            }
+          )
+          .then(function(response) {
+            var hbsObject = {
+              total: total
+            };
+            response.render("/Checkout", hbsObject);
+          });
+      });
+  });
+
+  // Get route for returning items of a specific category
   app.get("/items/category/:category", function(req, res) {
     // Add sequelize code to find all posts where the category is equal to req.params.category,
     // return the result to the user with res.json
+
     db.items
       .findAll({
         where: {
@@ -23,16 +60,17 @@ module.exports = function(app) {
         // console.log(dbitem)
       });
   });
+
   // PUT route for updating posts
   app.put("/api/items", function(req, res) {
     // Add code here to update a post using the values in req.body, where the id is equal to
     // req.body.id and return the result to the user using res.json
     db.item
       .updateOne({
-        name: req.params.name,
-        price: req.params.price,
-        category: req.params.category,
-        quantity: req.params.quantity,
+        name: req.body.name,
+        price: req.body.price,
+        category: req.body.category,
+        quantity: req.body.quantity,
 
         where: {
           id: req.params.id
