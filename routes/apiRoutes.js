44b8503310b00210656fc5ceafaee999b54e86c6
sahibaahuja <sahibaahuja>
2019-05-16 12:44:44 -0400

    updated navbar

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,8 +3,8 @@ var db = require("../models");
 module.exports = function(app) {
   // Get all examples
   app.get("/items", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+    db.item.findAll({}).then(function(dbitems) {
+      res.json(dbitems);
     });
   });
 
@@ -21,6 +21,25 @@ module.exports = function(app) {
       .then(function(dbitem) {
         res.json(dbitem);
         // console.log(dbitem)
+      });
+  });
+  // PUT route for updating posts
+  app.put("/api/items", function(req, res) {
+    // Add code here to update a post using the values in req.body, where the id is equal to
+    // req.body.id and return the result to the user using res.json
+    db.item
+      .updateOne({
+        name: req.params.name,
+        price: req.params.price,
+        category: req.params.category,
+        quantity: req.params.quantity,
+
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(function(dbitems) {
+        res.json(dbitems);
       });
   });
 };
