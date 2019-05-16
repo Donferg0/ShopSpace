802006eebad07f78f4c6c75bc07b81aab7c396fa
Annie Stewart <anniestewart@Annies-MacBook-Air.local>
2019-05-16 14:40:20 -0400

    checkout button js

--- public/js/index.js
+++ public/js/index.js
@@ -1,39 +1,42 @@
 $(document).ready(function() {
 
+  var shoppingCart = {};
+    $(document).on("click", ".checkout", 
+
   $(document).on("click", ".buy", handleBuyBtnClick);
 
+  // The API object contains methods for each kind of request we'll make
+  var API = {
+    buyItem: function(buy) {
+      return $.ajax({
+        headers: {
+          "Content-Type": "application/json"
+        },
+        type: "POST",
+        url: "api/purchase",
+        data: JSON.stringify(buy)
+      });
+    },
+    checkout: function(cart) {
+      return $.ajax({
+        headers: {
+          "Content-Type": "application/json"
+        },
+        type: "POST",
+        url: "api/checkout",
+        data: JSON.stringify(cart)
+      });
+    }
+  };
 
-// The API object contains methods for each kind of request we'll make
-var API = {
-  buyItem: function(buy) {
-    return $.ajax({
-      headers: {
-        "Content-Type": "application/json"
-      },
-      type: "POST",
-      url: "api/purchase",
-      data: JSON.stringify(buy)
-    });
-  },
-  // searchCategory: function() {
-  //   return $.ajax({
-  //     url: "api/items",
-  //     type: "GET"
-  //   });
-  // },
-  
-};
+  // handleDeleteBtnClick is called when an example's delete button is clicked
+  var handleBuyBtnClick = function() {
+    var idToBuy = $(this)
+      .parent()
+      .attr("data-id");
 
-// handleDeleteBtnClick is called when an example's delete button is clicked
-// Remove the example from the db and refresh the list
-var handleBuyBtnClick = function() {
-  var idToBuy = $(this)
-    .parent()
-    .attr("data-id");
+    API.buyItem(idToBuy).then(function() {});
+  };
+});
 
-  API.buyItem(idToBuy).then(function() {
-    
-  });
-};
 
-})
