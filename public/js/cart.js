77e76cc5488d92b2227dae1877250c78576591f0
Adam Pegram <adam@Adams-MacBook-Pro.local>
2019-05-17 19:26:45 -0400

    pushing it up

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,44 +1,44 @@
-//In Handlebars there will be plus and minus signs made for quantity that user wants to add to cart
-$(document).on("click", ".add-item", function () {
+$(document).ready(function() {
+  //In Handlebars there will be plus and minus signs made for quantity that user wants to add to cart
+  $(document).on("click", ".add-item", function () {
     var id = $(this).attr('data-name')
     var itemValue = ($(`#${id}`).text())
     itemValue++
     $(`#${id}`).text(itemValue)
-    var ele = $(this).parent().find('#addToCart')
+    var ele = $(this).parent().parent().find('#addToCart')
     ele.attr('data-item_quantity', itemValue)
-
-})
-
-$(document).on("click", ".subtract-item", function () {
+    ele.attr('data-item_name', $(this).parent().parent().find('#name2grab').text())  
+  })
+  
+  $(document).on("click", ".subtract-item", function () {
     var id = $(this).attr('data-name')
     var itemValue = ($(`#${id}`).text())
-    itemValue--
-    $(`#${id}`).text(itemValue)
-    var ele = $(this).parent().find('#addToCart')
-    ele.attr('data-item_quantity', itemValue)
-})
-
-var shoppingCart = JSON.parse(localStorage.getItem('shoppingCart')) || {};
-  var handleBuyBtnClick = function () {
-
-    //create any variables needed and grab data from what was clicked
-    //handlebars will need to populate any data attributes that you are trying to get information from
-    var name = $(this).attr('data-item_name')
-
-    //creating a blank object that will hold information on each item
-    var item_info = {}
-
-    item_info.price = $(this).attr('data-item_price')
-    item_info.purchase_quantity = $(this).attr('data-item_quantity')
-
-    shoppingCart = {
-      //utilize the spread operator in order to keep the current state of the shoppingCart, and then update the new item
-      ...shoppingCart,
-      // [variable] creates a variable that is what ever the name of the above var name variable is.
-      [name]: item_info
+    if (parseInt(itemValue) === 0){
+      return
+    } else {
+      itemValue--
+      $(`#${id}`).text(itemValue)
+      var ele = $(this).parent().find('#addToCart')
+      ele.attr('data-item_quantity', itemValue)
     }
 
-    //setting localStorage to hold shopping cart content for persistance
-    //must store object in local storage as a string for functionality
-    localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart))
-}
+  })
+  
+  function goToCart(cart) {
+    console.log('firing API')
+    return $.ajax({
+      headers: {
+        "Content-Type": "application/json"
+      },
+      type: "POST",
+      url: "/api/checkouts",
+      data: JSON.stringify(cart)
+    });
+  }
+  
+  $(document).on("click", ".goToCart", function () { 
+    var myCart = JSON.parse(localStorage.getItem('shoppingCart'))
+    console.log(myCart)
+    goToCart(myCart)
+  })
+})
