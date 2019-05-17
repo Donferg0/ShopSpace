417ffff3b9d2958ca2f8290eaeb5b6e4a27709c0
Tyawo <48452575+Tyawo@users.noreply.github.com>
2019-05-16 20:17:42 -0400

    update buttons, checkout

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -14,7 +14,7 @@ module.exports = function(app) {
   });
 
   app.get("/checkout", function(req, res) {
-    res.render("checkoutForm.html");
+    res.render("checkout");
   });
 
   // Render 404 page for any unmatched routes
