ef2964f5aeb8f129c11b7798daeadd669dc348e4
sahibaahuja <sahibaahuja>
2019-05-17 14:49:49 -0400

    update

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,39 @@
+var session = require("express-session");
+var db = require("../models");
+
 module.exports = function(app) {
+  app.use(
+    session({
+      secret: "keyboard cat"
+    })
+  );
+
   // Load index page
   app.get("/", function(req, res) {
     res.render("index");
+  });
+
+  app.get("/register", function(req, res) {
+    if (req.session.isAuthenticated) {
+      res.redirect("/");
+    }
+
+    res.render("signup");
+  });
+
+  app.post("/register", function(req, res) {
+    db.User.create({
+      email: req.body.email,
+      firstName: req.body.first,
+      lastName: req.body.last,
+      password: req.body.password
+    })
+      .then(function(created) {
+        res.redirect("/login");
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
   });
 
   // Load example page and pass in an example by id
@@ -9,7 +41,37 @@ module.exports = function(app) {
     res.render("login");
   });
 
-  app.get("/signup", function(req, res) {
+  app.post("/login", function(req, res) {
+    db.User.findOne({
+      where: {
+        email: req.body.email,
+        password: req.body.password
+      }
+    })
+      .then(function(found) {
+        if (!found) {
+          // show error to user
+          return;
+        }
+
+        req.session.isAuthenticated = true;
+        req.session.currentUser = found;
+
+        res.redirect("/");
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  });
+
+  app.get("/checkout", function(req, res) {
+    if (isAuthenticated) {
+      res.render("checkout");
+    } else {
+      res.render("/signup");
+    }
+    
+    app.get("/signup", function(req, res) {
     res.render("signup");
   });
 
@@ -21,4 +83,4 @@ module.exports = function(app) {
   app.get("*", function(req, res) {
     res.render("404");
   });
-};
+  };
