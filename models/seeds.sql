b2a495007b362bcc5c90352a301efaed60929bc2
Julie Morrison <jmor485@gmail.com>
2019-05-16 21:10:37 -0400

    modified seeds.sql

--- models/seeds.sql
+++ models/seeds.sql
@@ -1,13 +1,13 @@
-INSERT INTO items (name, price, quantity, category)
-VALUES ("Organic Marble Printed Shower Curtain", 49, 5, "Home"),
-("Silk Hand-Loomed Pillow Covers", 24.99, 40, "Home"),
-("Geometric Wall Hooks", 59, 20, "Home"),
-("GoWeave Essential Jumpsuit", 120, 5, "Clothing"),
-("Cotton-Linen Crew", 60, 100, "Clothing"),
-("Tie-Dye Box-Cut Tee", 25, 50, "Clothing"),
-("Soft Wool Rib Scarf", 65, 30, "Accessories"),
-("Mixed-Print Bandana", 18, 10, "Accessories"),
-("Acacia Kimono", 98, 8, "Accessories"),
-("Powder Blush in Peaches", 23, 100, "Beauty"),
-("Razor Sharp Liquid Eyeliner", 22, 30, "Beauty"),
-("Brow Power Universal Eyebrow Pencil", 24, 100, "Beauty");
+INSERT INTO items (name, price, quantity, category, createdAt, updatedAt)
+VALUES ("Organic Marble Printed Shower Curtain", 49, 5, "Home", "2019-05-05 15:59:21", "2019-05-05 15:59:21"),
+("Silk Hand-Loomed Pillow Covers", 24.99, 40, "Home", "2019-05-05 15:59:21", "2019-05-05 15:59:21"),
+("Geometric Wall Hooks", 59, 20, "Home", "2019-05-05 15:59:21", "2019-05-05 15:59:21"),
+("GoWeave Essential Jumpsuit", 120, 5, "Clothing", "2019-05-05 15:59:21", "2019-05-05 15:59:21"),
+("Cotton-Linen Crew", 60, 100, "Clothing", "2019-05-05 15:59:21", "2019-05-05 15:59:21"),
+("Tie-Dye Box-Cut Tee", 25, 50, "Clothing", "2019-05-05 15:59:21", "2019-05-05 15:59:21"),
+("Soft Wool Rib Scarf", 65, 30, "Accessories", "2019-05-05 15:59:21", "2019-05-05 15:59:21"),
+("Mixed-Print Bandana", 18, 10, "Accessories", "2019-05-05 15:59:21", "2019-05-05 15:59:21"),
+("Acacia Kimono", 98, 8, "Accessories", "2019-05-05 15:59:21", "2019-05-05 15:59:21"),
+("Powder Blush in Peaches", 23, 100, "Beauty", "2019-05-05 15:59:21", "2019-05-05 15:59:21"),
+("Razor Sharp Liquid Eyeliner", 22, 30, "Beauty", "2019-05-05 15:59:21", "2019-05-05 15:59:21"),
+("Brow Power Universal Eyebrow Pencil", 24, 100, "Beauty", "2019-05-05 15:59:21", "2019-05-05 15:59:21");
