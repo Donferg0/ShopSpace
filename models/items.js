a1a82659768466f9a4921475b0e218cd78045815
Julie Morrison <jmor485@gmail.com>
2019-05-17 14:52:29 -0400

    edited items.js

--- models/items.js
+++ models/items.js
@@ -3,10 +3,8 @@ module.exports = function(sequelize, DataTypes) {
     name: DataTypes.STRING,
     price: DataTypes.INTEGER,
     quantity: DataTypes.INTEGER,
-    category: DataTypes.TEXT,
+    category: DataTypes.TEXT
     // image: DataTypes.TEXT,
-    createdAt: DataTypes.DATE,
-    updatedAt: DataTypes.DATE
   });
 
   return items;
